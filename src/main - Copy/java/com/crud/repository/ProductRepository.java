4a9dcd8fbc2e621ca5c3476bb236769414902521
Shubham Mukund Lalge <96787417+shubhamlalge@users.noreply.github.com>
2023-03-24 18:27:56 +0530

    Delete src directory

--- src/main - Copy/java/com/crud/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
